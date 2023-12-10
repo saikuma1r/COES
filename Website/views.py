6616db8648e9c7f0a29baa7bd4894c2b2ac2e94b
ROKKAM SAIKUMAR <96229847+saikuma1r@users.noreply.github.com>
2023-12-10 16:30:35 +0000

    commit

--- Website/views.py
+++ Website/views.py
@@ -1,9 +1,14 @@
 from flask import Blueprint,render_template
+import matplotlib.pyplot as plt
+import seaborn as sns
+import io
+import base64
 
 views = Blueprint('views',__name__)
 @views.route('/')
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
+
 @views.route('/about')
 def about():
     return render_template("about.html")
@@ -18,4 +23,16 @@ def contact():
 
 @views.route('/data_load')
 def visual():
-    return render_template("visual.html")
+    data = sns.load_dataset("iris")
+    iris_data = sns.load_dataset("iris")
+    print(iris_data.head())
+    sns_plot = sns.pairplot(data, hue='species')
+
+    # Save the plot to a BytesIO object
+    img = io.BytesIO()
+    sns_plot.savefig(img, format='png')
+    img.seek(0)
+
+    # Encode the plot in base64 to display in HTML
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    return render_template("visual.html", plot_url=plot_url)
--- main.py
+++ main.py
@@ -2,6 +2,5 @@ from Website import create
 app = create()
 
 if __name__=='__main__':
-    #app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
     
